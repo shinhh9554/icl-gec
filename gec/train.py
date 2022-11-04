11dea9fe8d35066952eefd902ee7e3aab4c909f1
신현호 <shinhh9554@gmail.com>
2022-11-04 21:49:33 +0900

    Update gec train.py

--- gec/train.py
+++ gec/train.py
@@ -1,30 +1,51 @@
-import argparse
-import os
-import random
 import torch
-
-from utils.metric import Metric
-from utils.dataset import Loader
-from utils.preprocessor import Preprocessor
+import argparse
 
 import numpy as np
+from datasets import load_dataset, load_metric
 from transformers import (
-	AutoConfig,
-	AutoTokenizer,
-	TrainingArguments,
-	Trainer,
-	DataCollatorForTokenClassification,
+	PreTrainedTokenizerFast,
+	BartForConditionalGeneration,
+	DataCollatorForSeq2Seq,
+	Seq2SeqTrainingArguments,
+	set_seed,
+	Seq2SeqTrainer
 )
-
-import yaml
-import wandb
-from dotenv import load_dotenv
-from easydict import EasyDict
-from model import RobertaWithLinearHead
 
 
 def parse_args():
 	parser = argparse.ArgumentParser()
+
+	# Model arguments
+	parser.add_argument("--model_name_or_path", default='gogamza/kobart-base-v2', type=str)
+
+	# Dataset arguments
+	parser.add_argument("--train_file", default='data/gec_train.jsonl', type=str)
+	parser.add_argument("--validation_file", default='data/gec_valid.jsonl', type=str)
+	parser.add_argument("--max_input_length", default=128, type=int)
+	parser.add_argument("--max_target_length", default=128, type=int)
+	parser.add_argument("--preprocessing_num_workers", default=1, type=int)
+
+	# Training arguments
+	parser.add_argument("--seed", default=42, type=int)
+	parser.add_argument("--output_dir", default='output', type=str)
+	parser.add_argument("--num_train_epochs", default=5.0, type=float)
+	parser.add_argument("--per_device_train_batch_size", default=100, type=int)
+	parser.add_argument("--per_device_eval_batch_size", default=100, type=int)
+	parser.add_argument("--gradient_accumulation_steps", default=1, type=int)
+	parser.add_argument("--learning_rate", default=5e-5, type=float)
+	parser.add_argument("--weight_decay", default=0.001, type=float)
+	parser.add_argument("--warmup_ratio", default=0.05, type=float)
+	parser.add_argument("--fp16", default=True, type=bool)
+	parser.add_argument("--evaluation_strategy", default='epoch', type=str)
+	parser.add_argument("--logging_steps", default=50, type=int)
+	parser.add_argument("--save_strategy", default='epoch', type=str)
+	parser.add_argument("--load_best_model_at_end", default=True, type=bool)
+	parser.add_argument("--metric_for_best_model", default='bleu', type=str)
+
+	args = parser.parse_args()
+
+	return args
 
 
 def main():
@@ -32,90 +53,114 @@ def main():
 	args = parse_args()
 
 	# Seed
-	seed_everything(args.seed)
+	set_seed(args.seed)
 
 	# Loading Datasets
-	raw_datasets = load_dataset()
+	data_files = {"train": args.train_file, "validation": args.validation_file}
+	raw_datasets = load_dataset('json', data_files=data_files)
+
+	# Load pretrained model and tokenizer
+	tokenizer = PreTrainedTokenizerFast.from_pretrained(args.model_name_or_path)
+	model = BartForConditionalGeneration.from_pretrained(args.model_name_or_path)
 
 	# Preprocessing Datasets
-	preprocessor = Preprocessor()
-	datasets = datasets.map(preprocessor, batched=True)
+	def preprocess_function(examples):
+		inputs = examples["error"]
+		targets = examples["correct"]
 
-	# Config & Model
-	config = AutoConfig.from_pretrained(CFG.PLM)
-	config.num_labels = CFG.num_labels
+		# Tokenize inputs/targets
+		model_inputs = tokenizer(inputs, max_length=args.max_input_length, truncation=True)
+		labels = tokenizer(text_target=targets, max_length=args.max_target_length-1, truncation=True)
 
-	model = RobertaWithLinearHead.from_pretrained(CFG.PLM, config=config)
+		# add eos_token_id
+		model_inputs["labels"] = [target_ids + [tokenizer.eos_token_id] for target_ids in labels["input_ids"]]
+		return model_inputs
 
-	# Tokenizer
-	tokenizer = AutoTokenizer.from_pretrained(CFG.PLM)
-
-	# Data Collator
-	data_collator = DataCollatorForTokenClassification(tokenizer)
-
-	# Wandb
-	model_name = CFG.PLM.replace("/", "_")
-
-	load_dotenv(dotenv_path="wandb.env")
-	WANDB_AUTH_KEY = os.getenv('WANDB_AUTH_KEY')
-	wandb.login(key=WANDB_AUTH_KEY)
-
-	run_name = f"{model_name}-finetuned-ner"
-	wandb.init(entity=CFG.entity_name, project=CFG.project_name, name=run_name)
-
-	# Train & Eval configs
-	training_args = TrainingArguments(
-		run_name,
-		num_train_epochs=CFG.num_epochs,
-		per_device_train_batch_size=CFG.train_batch_size,
-		per_device_eval_batch_size=CFG.valid_batch_size,
-		gradient_accumulation_steps=CFG.gradient_accumulation_steps,
-		learning_rate=CFG.learning_rate,
-		weight_decay=CFG.weight_decay,
-		warmup_ratio=CFG.warmup_ratio,
-		fp16=CFG.fp16,
-		evaluation_strategy=CFG.evaluation_strategy,
-		save_steps=CFG.save_steps,
-		eval_steps=CFG.eval_steps,
-		logging_steps=CFG.logging_steps,
-		save_strategy=CFG.save_strategy,
-		save_total_limit=CFG.num_checkpoints,
-		load_best_model_at_end=CFG.load_best_model_at_end,
-		metric_for_best_model=CFG.metric_for_best_model,
+	# Train dataset
+	train_dataset = raw_datasets["train"]
+	train_dataset = train_dataset.map(
+		preprocess_function,
+		batched=True,
+		num_proc=args.preprocessing_num_workers,
+		desc="Running tokenizer on train dataset",
 	)
 
-	wandb.config.update(training_args)
+	# Valid dataset
+	eval_dataset = raw_datasets["validation"]
+	eval_dataset = eval_dataset.map(
+		preprocess_function,
+		batched=True,
+		num_proc=args.preprocessing_num_workers,
+		desc="Running tokenizer on validation dataset",
+	)
+
+	# Data Collator
+	data_collator = DataCollatorForSeq2Seq(tokenizer, model)
+
+	# Set warmup steps
+	total_batch_size = args.per_device_train_batch_size * torch.cuda.device_count()
+	n_total_iterations = int(len(train_dataset) / total_batch_size * args.num_train_epochs)
+	warmup_steps = int(n_total_iterations * args.warmup_ratio)
+
+	# Train & Eval configs
+	training_args = Seq2SeqTrainingArguments(
+		output_dir=args.output_dir,
+		num_train_epochs=args.num_train_epochs,
+		per_device_train_batch_size=args.per_device_train_batch_size,
+		per_device_eval_batch_size=args.per_device_eval_batch_size,
+		gradient_accumulation_steps=args.gradient_accumulation_steps,
+		learning_rate=args.learning_rate,
+		weight_decay=args.weight_decay,
+		warmup_ratio=args.warmup_ratio,
+		warmup_steps=warmup_steps,
+		fp16=args.fp16,
+		evaluation_strategy=args.evaluation_strategy,
+		logging_steps=args.logging_steps,
+		save_strategy=args.save_strategy,
+		load_best_model_at_end=args.load_best_model_at_end,
+		metric_for_best_model=args.metric_for_best_model,
+		predict_with_generate=True,
+	)
 
 	# Metrics
-	metrics = Metric()
+	metric = load_metric("sacrebleu")
+	def compute_metrics(eval_preds):
+		preds, labels = eval_preds
+		if isinstance(preds, tuple):
+			preds = preds[0]
+
+		decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+
+		labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+		decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+		decoded_preds = [pred.strip() for pred in decoded_preds]
+		decoded_labels = [[label.strip()] for label in decoded_labels]
+
+		result = metric.compute(predictions=decoded_preds, references=decoded_labels)
+		result = {"bleu": result["score"]}
+
+		return result
 
 	# Trainer
-	trainer = Trainer(
-		model,
-		training_args,
-		train_dataset=datasets["train"],
-		eval_dataset=datasets["validation"],
-		data_collator=data_collator,
+	trainer = Seq2SeqTrainer(
+		model=model,
+		args=training_args,
+		train_dataset=train_dataset,
+		eval_dataset=eval_dataset,
 		tokenizer=tokenizer,
-		compute_metrics=metrics.compute_metrics,
+		data_collator=data_collator,
+		compute_metrics=compute_metrics
 	)
 
 	# Training
 	trainer.train()
-	# Evaluating
-	trainer.evaluate()
-	wandb.finish()
+	trainer.save_model()
 
-
-def seed_everything(seed):
-	torch.manual_seed(seed)
-	torch.cuda.manual_seed(seed)
-	torch.cuda.manual_seed_all(seed)
-	torch.backends.cudnn.deterministic = True
-	torch.backends.cudnn.benchmark = False
-	np.random.seed(seed)
-	random.seed(seed)
-
+	# Evaluation
+	max_length = training_args.generation_max_length
+	num_beams = training_args.generation_num_beams
+	trainer.evaluate(max_length=max_length, num_beams=num_beams, metric_key_prefix="eval")
 
 if __name__ == '__main__':
 	main()
